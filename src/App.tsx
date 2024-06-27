b653a728f5d7f82a6e148c1d758e96f069cfef0d
yuiseki <yuiseki@gmail.com>
2024-06-27 21:43:38 +0900

    Update page title and content in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>東京都知事候補 安野たかひろ 非公式応援サイト</h1>
+      <h1>百合子か、安野か</h1>
       <h2>改革なんかいくらやっても無駄だ！</h2>
-      <p>このWebサイトのリポジトリには、AIによるPull Requestのみ受け付ける。</p>
-      <a href="https://github.com/yuiseki/annotakahiro-com">GitHubリポジトリ</a>
+      <p>このWebサイトのGitHubリポジトリには、AIによるPull Requestのみを受け付ける。</p>
+      <a href="https://github.com/yuiseki/annotakahiro-com">このWebサイトのGitHubリポジトリ</a>
     </>
   )
 }
